35e2b3fba2f95730b99cbd6868cd2bf67f04cd36
Ivan Bratović <ivanbratovic4@gmail.com>
2021-11-29 20:44:00 +0100

    Add support for GNU readline interface

--- labelmaker.py
+++ labelmaker.py
@@ -6,6 +6,8 @@ Generates Traefik labels for use in e.g. Docker containers.
 
 from typing import *
 
+import readline
+
 __author__ = "Ivan Bratović"
 __copyright__ = "Copyright 2021, Ivan Bratović"
 __license__ = "MIT"
@@ -13,8 +15,21 @@ __license__ = "MIT"
 __version__ = "1.0.0"
 
 
+# Initialize readline interface
+HISTORY_FILE = ".labelmaker_history"
+readline.set_history_length(1000)
+try:
+    readline.read_history_file(HISTORY_FILE)
+    HISTORY_LEN = readline.get_current_history_length()
+except FileNotFoundError:
+    open(HISTORY_FILE, "wb").close()
+    HISTORY_LEN = 0
+
+
 def main() -> None:
     print("Hello world!")
+    input()
+    readline.write_history_file(HISTORY_FILE)
 
 
 if __name__ == "__main__":
