f34c3c466a75758e30e692e82cd0c07f0a2395ae
Ivan Bratović <ivanbratovic4@gmail.com>
2021-11-29 20:17:47 +0100

    Add 'Hello world' example to test Git systems

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# labelmaker
+# Labelmaker
+
 Tool for generating Traefik labels
+
+## For developers
+
+Install all requirements before contributing. This is required for `pre-commit`.
+
+Before commiting, install pre-commit hooks for Git by running:
+```
+pre-commit install
+```
+
+This will run Black, MyPy and PyLint before commiting.
---
+++ labelmaker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Generates Traefik labels for use in e.g. Docker containers.
+"""
+
+from typing import *
+
+__author__ = "Ivan Bratović"
+__copyright__ = "Copyright 2021, Ivan Bratović"
+__license__ = "MIT"
+
+__version__ = "1.0.0"
+
+
+def main() -> None:
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
