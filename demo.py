85160db4c4cd03c36888509c6744c272a7d78a10
Ivan Bratović <ivanbratovic4@gmail.com>
2022-01-28 15:16:35 +0100

    move app demo to separate script
    Signed-off-by: Ivan Bratović <ivanbratovic4@gmail.com>

---
+++ demo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from typing import *
+from util.label import *
+
+
+def main() -> None:
+    print("\n # Simple service with an explicit port\n")
+    cfg = ServiceConfig(
+        name="test_service", rule=Rule("Host", ["example.com"]), port=25565
+    )
+    labels = gen_simple_label_set_for_service(cfg)
+    print("labels:", *labels, sep="\n  - ")
+
+    print("\n # Simple service with HTTPS redirect\n")
+    cfg = ServiceConfig(
+        name="test_service",
+        rule=Rule("Host", ["example.com"]),
+        https_redir=True,
+        tls_resolver="letsencrypt",
+    )
+    labels = gen_simple_label_set_for_service(cfg)
+    print("labels:", *labels, sep="\n  - ")
+
+    print("\n # Read from docker-compose.yml\n")
+    labels = gen_label_set_from_compose("docker-compose.yml")
+    print("labels:", *labels, sep="\n  - ")
+
+
+if __name__ == "__main__":
+    main()
--- labelmaker.py
+++ labelmaker.py
@@ -29,24 +29,6 @@ except FileNotFoundError:
 
 
 def main() -> None:
-    # Simple service with an explicit port
-    cfg = ServiceConfig(
-        name="test_service", rule=Rule("Host", ["example.com"]), port=25565
-    )
-    labels = gen_simple_label_set_for_service(cfg)
-    print("labels:", *labels, sep="\n  - ")
-    # Simple service with HTTPS redirect
-    cfg = ServiceConfig(
-        name="test_service",
-        rule=Rule("Host", ["example.com"]),
-        https_redir=True,
-        tls_resolver="letsencrypt",
-    )
-    labels = gen_simple_label_set_for_service(cfg)
-    print("labels:", *labels, sep="\n  - ")
-    # Read from docker-compose.yml
-    labels = gen_label_set_from_compose("docker-compose.yml")
-    print("labels:", *labels, sep="\n  - ")
     readline.write_history_file(HISTORY_FILE)
 
 
