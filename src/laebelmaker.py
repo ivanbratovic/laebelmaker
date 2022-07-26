9cf7dc81379c8a2f199e255f6b933eafd818ad9e
Ivan Bratović <ivanbratovic4@gmail.com>
2022-07-26 09:00:08 +0200

    laebelmaker.py: remove readline history initialization

--- src/laebelmaker.py
+++ src/laebelmaker.py
@@ -9,7 +9,6 @@ from typing import *
 from util.label import *
 from util.formatter import *
 
-import readline
 import argparse
 
 __author__ = "Ivan Bratović"
@@ -67,16 +66,6 @@ def main() -> Optional[NotImplementedType]:
 
     if args.interactive:
         return NotImplemented
-        # Initialize readline interface
-        HISTORY_FILE = ".laebelmaker_history"
-        readline.set_history_length(1000)
-        try:
-            readline.read_history_file(HISTORY_FILE)
-            HISTORY_LEN = readline.get_current_history_length()
-        except FileNotFoundError:
-            open(HISTORY_FILE, "wb").close()
-            HISTORY_LEN = 0
-        readline.write_history_file(HISTORY_FILE)
     if args.docker_compose:
         try:
             labels += gen_label_set_from_compose(args.docker_compose)
